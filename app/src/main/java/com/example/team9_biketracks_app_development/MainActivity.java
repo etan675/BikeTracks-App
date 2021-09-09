afa3b704d2f746d1b02a162da8cb9cb5ccae6c4e
shuyi-wang1 <shuyi.wang2000@gmail.com>
2021-09-09 13:36:47 +1200

    Added a dropdown menu for mode of transport

--- app/src/main/java/com/example/team9_biketracks_app_development/MainActivity.java
+++ app/src/main/java/com/example/team9_biketracks_app_development/MainActivity.java
@@ -5,16 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     Intent sessionIntent;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //Dropdown menu for the activity chosen
+        Spinner spinner = findViewById(R.id.activity_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.activity, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        //Start button
         Button startSessionButton = (Button) findViewById(R.id.startSessionButton);
         startSessionButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +42,17 @@ public class MainActivity extends AppCompatActivity {
     public void openStartSession() {
         sessionIntent = new Intent(this, ActivityManager.class);
         startActivity(sessionIntent);
+    }
+
+    //Dropdown menu methods
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
