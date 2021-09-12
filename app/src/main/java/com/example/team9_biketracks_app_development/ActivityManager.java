4bf6d8ad239131b4412cbdd4cf99029b76d1d98e
Eric <erictang1201@gmail.com>
2021-09-12 19:24:26 +1200

    added gps sensor

--- app/src/main/java/com/example/team9_biketracks_app_development/ActivityManager.java
+++ app/src/main/java/com/example/team9_biketracks_app_development/ActivityManager.java
@@ -1,12 +1,18 @@
 package com.example.team9_biketracks_app_development;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.util.Log;
@@ -16,17 +22,28 @@ import android.widget.Chronometer;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.io.IOException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.io.IOException;
 
 /** ActivityManager class. */
 public class ActivityManager extends AppCompatActivity implements SensorEventListener {
+    public static final int DEFAULT_UPDATE_INTERVAL = 30;
+    public static final int FASTEST_UPDATE_INTERVAL= 5;
     /** Logger. */
     private static final String LOGGER = "StartSession";
+    private static final int FINE_LOCATION_PERMISSION = 99 ;
     /** SessionManager instance. */
     private SensorManager sensorManager;
+
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationRequest locationRequest;
+
     /** Sensor fields. */
-    EditText acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, mag_x, mag_y, mag_z;
+    EditText acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, mag_x, mag_y, mag_z, latitude, longitude, altitude;
     /** Store old sensor fields */
     float old_acc_x = 1.0f;
     float old_acc_y = 1.0f;
@@ -69,10 +86,22 @@ public class ActivityManager extends AppCompatActivity implements SensorEventLis
         mag_x = findViewById(R.id.xMValue);
         mag_y = findViewById(R.id.yMValue);
         mag_z = findViewById(R.id.zMValue);
+        latitude = findViewById(R.id.latitudeValue);
+        longitude = findViewById(R.id.longitudeValue);
+        altitude = findViewById(R.id.altitudeValue);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         Sensor gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         Sensor magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        locationRequest = LocationRequest.create();
+
+        locationRequest.setInterval(1000 * DEFAULT_UPDATE_INTERVAL);
+
+        locationRequest.setFastestInterval(1000 * FASTEST_UPDATE_INTERVAL);
+
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
         // register sensors
         if (accelerometer != null) {
             sensorManager.registerListener(ActivityManager.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
@@ -88,7 +117,58 @@ public class ActivityManager extends AppCompatActivity implements SensorEventLis
             sensorManager.unregisterListener(this);
             finish();
         });
+        updateGPS();
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case FINE_LOCATION_PERMISSION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    updateGPS();
+                } else {
+                    Toast.makeText(this, "This app requires location permission to be granted", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+        }
+    }
+
+    private void updateGPS() {
+        //get permissions to track GPS
+        //get location from fused client provider
+        //update UI
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(ActivityManager.this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>(){
+                @Override
+                public void onSuccess(Location location) {
+                    // got permissions, need to put values into UI
+                    updateUIValues(location);
+                }
+            });
+        }
+        else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_PERMISSION);
+            }
+        }
+    }
+
+    private void updateUIValues(Location location) {
+        latitude.setText(String.valueOf(location.getLatitude()));
+        longitude.setText(String.valueOf(location.getLongitude()));
+        if (location.hasAltitude()) {
+            altitude.setText(String.valueOf(location.getAltitude()));
+        }
+        else{
+            altitude.setText("Not available");
+        }
+    }
+
 
     public void startChronometer(View v) throws IOException {
         if (!active) {
