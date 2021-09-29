806b509f6088426ddc66a6c5e7c33618a5bc2951
shuyi-wang1 <shuyi.wang2000@gmail.com>
2021-09-29 16:46:40 +1300

    Load map on route page and add location to NZ

---
+++ app/src/main/java/com/example/team9_biketracks_app_development/MapActivity.java
@@ -0,0 +1,39 @@
+package com.example.team9_biketracks_app_development;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_route);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+        LatLng auckland = new LatLng(-36.8509, 174.7645);
+        googleMap.addMarker(new MarkerOptions()
+                .position(auckland)
+                .title("Auckland"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(auckland));
+
+
+    }
+}
--- app/src/main/java/com/example/team9_biketracks_app_development/fragment/RouteFragment.java
+++ app/src/main/java/com/example/team9_biketracks_app_development/fragment/RouteFragment.java
@@ -1,5 +1,6 @@
 package com.example.team9_biketracks_app_development.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,13 +10,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.team9_biketracks_app_development.MapActivity;
 import com.example.team9_biketracks_app_development.R;
+import com.example.team9_biketracks_app_development.SensorActivity;
 
 public class RouteFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        startActivity(new Intent(getActivity(), MapActivity.class));
         return inflater.inflate(R.layout.fragment_route, container, false);
     }
 }
